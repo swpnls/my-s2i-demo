4211e0e94e8d4ef3889771f30f3cf108675ac882
swpnls <swapnil.shrivastava@hitachivantara.com>
2026-09-23 15:37:26 +0800

    Update home route response message

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello! This app was built using S2I (Source-to-Image). No Dockerfile was written!"
+    return "I changed the code"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
